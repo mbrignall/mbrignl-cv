019604401694132910b57799fa30890f72d2e68a
mbrignall <martin.brignall@ovo.com>
2024-09-02 15:38:47 +0100

    removing pandoc and amending script

--- build-site.el
+++ build-site.el
@@ -12,11 +12,9 @@
 
 ;; Install dependencies
 (package-install 'htmlize)
-(package-install 'ox-pandoc) ;; Install ox-pandoc for Org to Pandoc conversion
 
 ;; Load the publishing system
 (require 'ox-publish)
-(require 'ox-pandoc)
 
 ;; Customize the HTML output
 (setq org-html-validation-link nil            ;; Don't show validation link
@@ -45,22 +43,5 @@
 
 ;; Generate the site output
 (org-publish-all t)
-
-;; Open the Org file in an Org-mode buffer and export to PDF
-(let ((org-file "./content/index.org")  ;; Path to your Org file
-      (output-dir "./")           ;; Output directory for the PDF
-      (pdf-file "index.pdf"))            ;; Output PDF file name
-  (if (and (file-exists-p org-file)
-           (executable-find "pandoc"))
-      (with-current-buffer (find-file-noselect org-file) ;; Open file in a buffer
-        (org-mode)  ;; Ensure buffer is in Org mode
-        ;; Export Org file to PDF using ox-pandoc via LaTeX
-        (org-pandoc-export-to-latex-pdf)
-        ;; Move the generated PDF to the correct output directory
-        (let ((generated-pdf (concat (file-name-sans-extension org-file) ".pdf")))
-          (if (file-exists-p generated-pdf)
-              (rename-file generated-pdf (expand-file-name pdf-file output-dir) t)
-            (message "Error: PDF generation failed; file does not exist: %s" generated-pdf))))
-    (message "Error: Either Org file does not exist or Pandoc is not installed.")))
 
 (message "Build complete!")
